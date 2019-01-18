db71329087fd5423cc08d09248fe9d021730850f
tweeki99 <danil.shipilov.99@gmail.com>
2019-01-18 22:33:46 +0600

    hw7

---
+++ Day7 (Tanks)/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib.WorldOfTanks;
+
+namespace Day7__Tanks_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityTanksInArmy = 5;
+
+            Tank[] firstTanksArmy = new Tank[quantityTanksInArmy];
+            Tank[] secondTanksArmy = new Tank[quantityTanksInArmy];
+
+            for (int i = 0; i < quantityTanksInArmy; i++)
+            {
+                firstTanksArmy[i] = new Tank("T-34");
+                secondTanksArmy[i] = new Tank("Pantera");
+            }
+
+            int index = 0;
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine(firstTanksArmy[index].GetTankParameters() + "\n");
+
+                    Console.WriteLine(secondTanksArmy[index].GetTankParameters() + "\n");
+
+                    Console.WriteLine(firstTanksArmy[index] * secondTanksArmy[index]);
+
+                    index++;
+
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.Clear();
+                Console.WriteLine("Конец битвы");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WorldOfTanks
+{
+    public class Tank
+    {
+        public string ModelTank { get; set; }
+        public int Ammunition { get; set; }
+        public int Armor { get; set; }
+        public int Maneuverability { get; set; }
+
+        static private Random randomizer = new Random();
+
+        public Tank(string modelTank)
+        {
+            ModelTank = modelTank;          
+            Ammunition = randomizer.Next(0, 101);
+            Armor = randomizer.Next(0, 101);
+            Maneuverability = randomizer.Next(0, 101);
+        }
+
+        public static string operator *(Tank firstTank, Tank secondTank)
+        {
+            int pointsFirstTank = 0, pointsSecondTank = 0;
+
+            if (firstTank.Armor > secondTank.Armor)
+            {
+                pointsFirstTank++;
+            }
+
+            if (secondTank.Armor > firstTank.Armor)
+            {
+                pointsSecondTank++;
+            }
+
+            if (firstTank.Ammunition > secondTank.Ammunition)
+            {
+                pointsFirstTank++;
+            }
+
+            if (secondTank.Ammunition > firstTank.Ammunition)
+            {
+                pointsSecondTank++;
+            }
+
+            if (firstTank.Maneuverability > secondTank.Maneuverability)
+            {
+                pointsFirstTank++;
+            }
+
+            if (secondTank.Maneuverability > firstTank.Maneuverability)
+            {
+                pointsSecondTank++;
+            }
+
+            if (pointsFirstTank - pointsSecondTank >= 1)
+            {
+                return "Победил " + firstTank.ModelTank;
+            }
+            else if (pointsSecondTank - pointsFirstTank >= 1)
+            {
+                return "Победил " + secondTank.ModelTank;
+            }
+            else
+            {
+                return "Ничья";
+            }
+        }
+
+        public string GetTankParameters()
+        {
+            return "Модель танка: " + ModelTank + "\nУровень боекомплекта: " + Ammunition + "%\nУровень брони: " + Armor + "%\nУровень маневренности: " + Maneuverability + "%\n";
+        }    
+    }
+}
